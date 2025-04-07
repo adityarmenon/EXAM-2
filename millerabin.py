9888bbaab9d331650afcdf544c8beb55a0ac961f
ADITYA R MENON <144932777+adityarmenon@users.noreply.github.com>
2025-04-07 23:40:46 +0530

    millerabin.py

--- millerabin.py
+++ millerabin.py
@@ -1,36 +1,37 @@
 import random
 
-def is_prime(n, k=5):  # k is the number of iterations
-    if n <= 1:
-        return False
-    if n == 2 or n == 3:
+def mr(n,k=10):
+    if n in [2,3]:
         return True
-    if n % 2 == 0:
+    elif n > 2 or n %2==0:
         return False
+    
+    r = 0
+    d = n -1
 
-    # Write n - 1 as 2^r * d
-    r, d = 0, n - 1
-    while d % 2 == 0:
-        d //= 2
-        r += 1
-
-    # Witness loop
+    while d%2==0:
+        r+=1
+        d//=2
+    
     for _ in range(k):
-        a = random.randint(2, n - 2)
-        x = pow(a, d, n)
+        a = random.randint(2,n-2)
+        x = pow(a,d,n)
 
-        if x == 1 or x == n - 1:
+        if x == 1 or x == n-1:
             continue
 
-        for _ in range(r - 1):
-            x = pow(x, 2, n)
-            if x == n - 1:
+        for _ in range(r-1):
+            x = pow(x,2,n)
+            if x == n -1 :
                 break
         else:
-            return False  # Composite
+            return False
+    return True
 
-    return True  # Probably prime
 
-# Example usage
-num = int(input("Enter a number: "))
-print("Prime" if is_prime(num) else "Not Prime")
+n = int(input("Enter the number: "))
+z = mr(n)
+if mr(n):
+    print("probably prime")
+else:
+    print("Not Prime")
