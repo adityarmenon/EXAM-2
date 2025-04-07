263037b3e782f8b4b8c831c88ba0d8cf144973ea
ADITYA R MENON <144932777+adityarmenon@users.noreply.github.com>
2025-04-07 23:44:14 +0530

    list.py

--- list.py
+++ list.py
@@ -1,7 +1,8 @@
 import os
-folder = " /home/arm/Downloads"
-extensions = (".jpg, .pdf, .png,")
+
+folder = "/home/arm/Downloads"
+extensions = (".jpg", ".pdf", ".png")
 
 for file in os.listdir(folder):
-    if file.lower().endswit(extensions)
-    print(file)
+    if file.lower().endswith(extensions):
+        print(file)
